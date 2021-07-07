440be1e609e5fd839a50bc067f751565f71ce377
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-07-07 14:18:28 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import AddressBookForm from "./components/address-book-form/address-book-form";
+import HomePage from "./components/address-book-home/address-book-home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+        <Switch>
+          <Route exact path = "/address-book-form"><AddressBookForm /></Route>
+          <Route exact path = "/home"><HomePage /></Route>
+          <Route exact path="/address-book-form/:id"><AddressBookForm /></Route>
+          <Route exact path=""><Redirect exact from="/" to="/home" /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
